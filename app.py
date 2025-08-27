55bb5a828c066224456cb9192928201570a12835
Aziz Tarous <56452240+aziztarous1999@users.noreply.github.com>
2025-08-27 16:29:32 +0100

    Connect To Mongodb

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import os
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+mongo = PyMongo(app)
+colis_collection = mongo.db.colis
+
+@app.route("/testConnection", methods=["GET"])
+def testConnection():
+    try:
+        mongo.db.command("ping")
+        return jsonify({"status": "success", "message": "MongoDB connected!"}), 200
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=True)
